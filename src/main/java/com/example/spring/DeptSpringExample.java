0c06cbfff906dd879200f05515ddc198888e6716
nto2 <nto2@naver.com>
2017-09-06 17:25:07 +0900

    1. spring-context library　추가 	- beans.xml 	- DeptExample ==> no framework(legacy) ==> Control 100% 	- DeptSpringExample ==> Client Code (5%), Framework(95%) ==> IoC

---
+++ src/main/java/com/example/spring/DeptSpringExample.java
@@ -0,0 +1,20 @@
+package com.example.spring;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class DeptSpringExample {
+
+	public static void main(String[] args) {
+		
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("classpath:/beans.xml");
+		
+		Dept dept = ctx.getBean(Dept.class);
+		
+		System.out.println(dept);
+		System.out.println(dept.getDeptno() + ", " + dept.getDname() + ", " + dept.getLoc());
+		
+		ctx.close();
+
+	}
+
+}
